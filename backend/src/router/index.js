058c55bc7b05be4935c8491da7ea11aa0f3b4d80
Hong Nhung <tranhongnhung668@gmail.com>
2023-01-26 21:48:25 +0700

    form login

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,6 +1,8 @@
 import {createRouter, createWebHistory} from 'vue-router';
-import Dashboard from '../views/Dashboard.vue'
-import Login from '../views/Login.vue'
+import Dashboard from '../views/Dashboard.vue';
+import Login from '../views/Login.vue';
+import RequestPassword from "../views/RequestPassword.vue";
+import ResetPassword from "../views/ResetPassword.vue";
 
 const routes = [
 	{
@@ -12,7 +14,23 @@ const routes = [
 		path: '/login',
 		name: 'login',
 		component: Login
-	}
+	},
+	{
+	    path: '/request-password',
+	    name: 'requestPassword',
+	    component: RequestPassword,
+	    meta: {
+	      requiresGuest: true
+	    }
+	},
+	{
+	    path: '/reset-password/:token',
+	    name: 'resetPassword',
+	    component: ResetPassword,
+	    meta: {
+	      requiresGuest: true
+	    }
+	},
 ];
 
 const router = createRouter({
